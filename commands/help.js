a6ab67e6635e45e65ecec3dcd8a529ba97ab20ae
javaruntimemc <yewshan.ooi@gmail.com>
2021-03-29 14:23:21 +0800

    Updated help.js

--- commands/help.js
+++ commands/help.js
@@ -12,19 +12,19 @@ module.exports = {
 		const { commands } = message.client;
 
 		if (!args.length) {
-			const embed = new MessageEmbed()
+			const embedUserSend = new MessageEmbed()
 				.setTitle('Help')
 				.setDescription(`You can send \`${prefix}help {command name}\` to get info on a specific command!`)
 				.addField('Commands', commands.map(command => command.name).join(', '))
 				.setColor(embedColor);
 
-			return message.author.send(embed)
+			return message.author.send(embedUserSend)
 				.then(() => {
 					if (message.channel.type === 'dm') return;
-					const embed2 = new MessageEmbed()
+					const embedChannelSend = new MessageEmbed()
 						.setDescription(`<@${message.author.id}>, I've sent you a DM with all the commands!`)
 						.setColor(embedColor);
-					message.reply(embed2);
+					message.reply(embedChannelSend);
 				})
 				.catch(error => {
 					message.channel.send(`It seems like I can't DM you!\n Error: \`${error.message}\``);
