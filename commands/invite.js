cebb60db1102af59468932ab8d8dd85391cedfd8
javaruntimemc <yewshan.ooi@gmail.com>
2021-04-14 20:27:54 +0800

    Updated to 0.3.1-beta

--- commands/invite.js
+++ commands/invite.js
@@ -9,7 +9,6 @@ module.exports = {
     execute (message) {
         const embed = new MessageEmbed()
             .setTitle('Invite')
-            .setDescription('SkyBot by SkyElements')
             .addField('Bot Invitation Link', '[*discord.com*](https://discord.com/api/oauth2/authorize?client_id=531811937244151808&permissions=8&scope=bot%20applications.commands)')
             .addField('Official Github Repository', '[*github.com*](https://github.com/javaruntimemc/skybot)')
             .addField('Official Website', '[*weebly.com*](https://skyelements.weebly.com)')
