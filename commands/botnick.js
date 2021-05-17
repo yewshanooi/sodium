7f1df50ce168199b9e73ec44367f5e80fa291ec8
Ooi Yew Shan <yewshan.ooi@gmail.com>
2021-05-17 10:46:31 +0800

    Added maximum .length for nick and botnick command

--- commands/botnick.js
+++ commands/botnick.js
@@ -9,16 +9,22 @@ module.exports = {
     guildOnly: true,
     execute (message, args) {
         if (!message.member.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('Error: You have no permission to use this command.');
+            const bot = message.guild.member(message.client.user);
 
             const nickname = args.join(' ');
                 if (!nickname) return message.channel.send('Error: Please provide a valid username.');
 
-            const embed = new MessageEmbed()
-                .setTitle('Bot Nickname')
-                .setDescription(`Nickname successfully changed to **${nickname}**`)
-                .setTimestamp()
-                .setColor(embedColor);
+            if (nickname.length <= '32') {
+                const embed = new MessageEmbed()
+                    .setTitle('Bot Nickname')
+                    .setDescription(`Nickname successfully changed to **${nickname}**`)
+                    .setTimestamp()
+                    .setColor(embedColor);
 
-            message.channel.send(embed).then(message.guild.member(message.client.user).setNickname(nickname));
+                message.channel.send(embed).then(bot.setNickname(nickname));
+            }
+            else {
+                return message.channel.send('Error: Nickname must be 32 characters or fewer.');
+            }
         }
-    };
+};
--- commands/nick.js
+++ commands/nick.js
@@ -10,18 +10,23 @@ module.exports = {
     execute (message, args) {
 		if (!message.member.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('Error: You have no permission to use this command.');
 			const user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-            const users = message.mentions.users.first();
                 if (user.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('Error: This user\'s nickname cannot be changed.');
+            const users = message.mentions.users.first();
 
-            const nickname = args.join(' ').slice(22);
-                if (!nickname) return message.channel.send('Error: Please provide a valid username.');
+                const nickname = args.join(' ').slice(22);
+                    if (!nickname) return message.channel.send('Error: Please provide a valid username.');
 
-            const embed = new MessageEmbed()
-                .setTitle('Nickname')
-                .setDescription(`**${users.username}**'s nickname successfully changed to **${nickname}**`)
-                .setTimestamp()
-                .setColor(embedColor);
+                if (nickname.length <= '33') {
+                    const embed = new MessageEmbed()
+                        .setTitle('Nickname')
+                        .setDescription(`**${users.username}**'s nickname successfully changed to **${nickname}**`)
+                        .setTimestamp()
+                        .setColor(embedColor);
 
-            message.channel.send(embed).then(user.setNickname(nickname));
-        }
+                    message.channel.send(embed).then(user.setNickname(nickname));
+                }
+                else {
+                    return message.channel.send('Error: Nickname must be 32 characters or fewer.');
+                }
+            }
     };
