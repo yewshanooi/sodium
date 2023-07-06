8b7a90bbe1fe8b0549b4d415f8389da71a325c63
Java <yewshan.ooi@gmail.com>
2023-07-06 14:16:08 +0800

    fix: fixed color.js error

--- commands/Fun/color.js
+++ commands/Fun/color.js
@@ -5,16 +5,15 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('color')
         .setDescription('Get a random color in HEX format'),
-    cooldown: '3',
+    cooldown: '0',
     category: 'Fun',
     guildOnly: false,
     async execute (interaction) {
         const Color = await fetch('https://www.colr.org/json/colors/random/1')
             .then(res => res.json());
 
-        if (Color.colors[0].hex === '') {
-            return interaction.reply({ content: 'Error: There was an error getting a random color.' });
-        }
+        if (Color.colors[0].hex === '') return interaction.reply({ content: 'Error: There was an error getting a random color.' });
+        if (Color.colors[0].tags.length === 0) return interaction.reply({ content: 'Error: There was an error getting a random color.' });
 
         const capitalizedName = Color.colors[0].tags[0].name.charAt(0).toUpperCase() + Color.colors[0].tags[0].name.slice(1);
 
