a81846ba7f44a3ed2d97a399ff60c8f4c0a48bc3
Java <yewshan.ooi@gmail.com>
2024-02-01 15:46:04 +0800

    fix: changed harm block threshold to BLOCK_NONE for gemini.js

--- commands/Utility/gemini.js
+++ commands/Utility/gemini.js
@@ -30,19 +30,19 @@ module.exports = {
 			const safetySettings = [
 				{
 					category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-					threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
+					threshold: HarmBlockThreshold.BLOCK_NONE
 				},
 				{
 					category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-					threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+					threshold: HarmBlockThreshold.BLOCK_NONE
 				},
 				{
 					category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-					threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+					threshold: HarmBlockThreshold.BLOCK_NONE
 				},
 				{
 					category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-					threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE
+					threshold: HarmBlockThreshold.BLOCK_NONE
 				}
 			];
 
