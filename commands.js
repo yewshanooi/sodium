4055612faf5ed16c7a6935bb7dd31c60847f4da0
Ooi Yew Shan <yewshan.ooi@gmail.com>
2024-02-04 23:05:27 +0800

    docs: fixed comments in commands.js

--- commands.js
+++ commands.js
@@ -16,11 +16,11 @@ for (const categories of commandsFolder) {
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
+// This line of code has been destructured
 const [, , option, commandName] = process.argv;
-// This line of code has been destructured.
 
 if (option === 'deploy') {
-    // By using deploy {command_name}, it will delete all exising commands and deploy the specified (one) command only.
+    // By using "node commands.js deploy {command_name}", it will delete all existing commands and only deploy a single command
     const body = commandName ? commands.filter(cmd => cmd.name === commandName) : commands;
     rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body })
         .then(data => console.log(`\nSuccessfully deployed ${chalk.bold(`${data.length}`)} application command(s)\n`))
@@ -32,13 +32,13 @@ else if (option === 'delete') {
         .catch(console.error);
 }
 else {
-    console.log(`${chalk.red.bold('[Error] Invalid option. Please use either \'deploy\' or \'delete\' as an option.')}`);
+    console.log(`${chalk.red.bold('[Error] Invalid option. Please use either \'deploy\', \'deploy {command_name}\', or \'delete\' as an option.')}`);
 }
 
 
 /*
  * Due to Discord API's limitation, you can only deploy a maximum of 200 commands in a single guild per day.
- * Trying to deploy more than 200 commands will cause the command to not execute.
+ * Trying to deploy more than 200 commands will cause the command to not execute properly.
  *
  * Commands will only be deployed/deleted for a single guild by default for development purpose.
  * Replace the specific line of code with the one given below to change this:
