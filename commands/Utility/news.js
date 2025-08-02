3292b0fb54acbdb22f220e62a2bd5dc06aa5d2ee
Ooi Yew Shan <yewshan.ooi@gmail.com>
2025-08-02 16:29:20 +0800

    feat: updated news sources for news command

--- commands/Utility/news.js
+++ commands/Utility/news.js
@@ -5,7 +5,29 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('news')
         .setDescription('Get the latest news from multiple sources')
-        .addStringOption(option => option.setName('source').setDescription('Select a news source').addChoices({ name: 'Al Jazeera English', value: 'al-jazeera-english' }, { name: 'BBC News', value: 'bbc-news' }, { name: 'CBS News', value: 'cbs-news' }, { name: 'CNN', value: 'cnn' }, { name: 'Reuters', value: 'reuters' }, { name: 'The Verge', value: 'the-verge' }, { name: 'The Wall Street Journal', value: 'the-wall-street-journal' }, { name: 'The Washington Post', value: 'the-washington-post' }).setRequired(true)),
+        .addStringOption(option => option.setName('source').setDescription('Select a news source')
+            .addChoices(
+                { name: 'ABC News', value: 'abc-news' }, 
+                { name: 'Al Jazeera English', value: 'al-jazeera-english' }, 
+                { name: 'Ars Technica', value: 'ars-technica' }, 
+                { name: 'Associated Press', value: 'associated-press' }, 
+                { name: 'Axios', value: 'axios' }, 
+                { name: 'BBC News', value: 'bbc-news' }, 
+                { name: 'Bloomberg', value: 'bloomberg' }, 
+                { name: 'CBS News', value: 'cbs-news' }, 
+                { name: 'CNN', value: 'cnn' }, 
+                { name: 'Financial Post', value: 'financial-post' }, 
+                { name: 'Fortune', value: 'fortune' }, 
+                { name: 'IGN', value: 'ign' }, 
+                { name: 'National Geographic', value: 'national-geographic' }, 
+                { name: 'TechCrunch', value: 'techcrunch' }, 
+                { name: 'The Verge', value: 'the-verge' }, 
+                { name: 'The Wall Street Journal', value: 'the-wall-street-journal' }, 
+                { name: 'The Washington Post', value: 'the-washington-post' },
+                { name: 'TIME', value: 'time' },
+                { name: 'VICE News', value: 'vice-news' },
+                { name: 'Wired', value: 'wired' }
+            ).setRequired(true)),
     cooldown: '15',
     category: 'Utility',
     guildOnly: false,
